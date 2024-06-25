429fe76f4d6b4ed229ad95f079cf8af43360bd8d
hafsa-wasi <118012663+hafsa-wasi@users.noreply.github.com>
2024-06-25 16:40:25 +0530

    Update dashboard.py

--- yt_dashboard_st/dashboard.py
+++ yt_dashboard_st/dashboard.py
@@ -42,7 +42,7 @@ def audience_simple(country):
 #LOAD DATA
 @st.cache_data
 def load_data():
-    df_agg = pd.read_csv('Aggregated_Metrics_By_Video.csv').iloc[1:,:]
+    df_agg = pd.read_csv('./Aggregated_Metrics_By_Video.csv').iloc[1:,:]
     df_agg.columns = ['Video','Video title','Video publish time','Comments added','Shares','Dislikes','Likes',
                           'Subscribers lost','Subscribers gained','RPM(USD)','CPM(USD)','Average % viewed','Average view duration',
                           'Views','Watch time (hours)','Subscribers','Your estimated revenue (USD)','Impressions','Impressions ctr(%)']
@@ -54,9 +54,9 @@ def load_data():
     df_agg['Engagement_ratio'] =  (df_agg['Comments added'] + df_agg['Shares'] +df_agg['Dislikes'] + df_agg['Likes']) /df_agg.Views
     df_agg['Views / sub gained'] = df_agg['Views'] / df_agg['Subscribers gained']
     df_agg.sort_values('Video publish time', ascending = False, inplace = True)    
-    df_agg_sub = pd.read_csv('Aggregated_Metrics_By_Country_And_Subscriber_Status.csv')
-    df_comments = pd.read_csv('All_Comments_Final.csv')
-    df_time = pd.read_csv('Video_Performance_Over_Time.csv')
+    df_agg_sub = pd.read_csv('./Aggregated_Metrics_By_Country_And_Subscriber_Status.csv')
+    df_comments = pd.read_csv('./All_Comments_Final.csv')
+    df_time = pd.read_csv('./Video_Performance_Over_Time.csv')
     df_time['Date'] = pd.to_datetime(df_time['Date'], format='mixed')
     return df_agg, df_agg_sub, df_comments, df_time
 
